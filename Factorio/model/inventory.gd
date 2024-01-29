b21ae605c3dc898973e979a5bb1aae9949f4e74a
JACKADUX <1505648942@qq.com>
2024-01-29 20:53:58 +0800

    inventory

--- Factorio/model/inventory.gd
+++ Factorio/model/inventory.gd
@@ -2,53 +2,54 @@ class_name Inventory
 
 signal invetory_changed
 
-var _slots :Array[InventorySlot]= []
+var _size :int = 0
+var _slots := {}
 
 ## Statics
 func _init(number:int=0) :
-	_slots.resize(number)
+	_size = number
 
 ## Interface
 # ----------------- slots
 func get_slots() -> Array[InventorySlot]:
-	return _slots
+	var array_slots :Array[InventorySlot] = [] 
+	array_slots.resize(_size)
+	for i in _slots:
+		array_slots[i] = _slots[i]
+	return array_slots
 
 func get_slot_count() -> int:
-	return _slots.size()
+	return _size
 
 func get_slot(index:int) -> InventorySlot:
-	return _slots[index]
+	return _slots.get(index)
 	
 func get_slot_with(item:BaseItem) -> InventorySlot:
-	for slot:InventorySlot in _slots:
-		if not slot:
-			continue
+	for slot:InventorySlot in _slots.values():
 		if slot.get_item() == item:
 			return slot
 	return null
 
 func add_slot(value:InventorySlot, index:int):
-	if value in _slots or _slots[index]:
+	if value in _slots.values() or _slots.has(index):
 		return 
 	_slots[index] = value
 	value.slot_changed.connect(_on_slot_changed.bind(value))
 	invetory_changed.emit()
 		
 func remove_slot(value:InventorySlot):
-	var index = _slots.find(value)
-	if index == -1:
+	var index = _slots.find_key(value)
+	if index == null:
 		return 
 	value.slot_changed.disconnect(_on_slot_changed.bind(value))
-	_slots[index] = null
+	_slots.erase(index)
 	invetory_changed.emit()
 	
 # ----------------- other
 func get_item_count_data() -> Dictionary:
 	""" { base_item : count } """
 	var data := {}
-	for slot:InventorySlot in _slots:
-		if not slot:
-			continue
+	for slot:InventorySlot in _slots.values():
 		var item :BaseItem = slot.get_item() 
 		if not data.has(item):
 			data[item] = 0
@@ -74,21 +75,13 @@ func _sort(sort_callable:= Callable()):
 	if not sort_callable:
 		sort_callable = func(a,b): return a.id < b.id
 	order_items.sort_custom(sort_callable)
-	var number = _slots.size()
-	_slots = []
+	_slots = {}
 	for i in order_items.size():
 		var item:BaseItem = order_items[i]
 		var slot = InventorySlot.new(item, count_data[item])
 		slot.slot_changed.connect(_on_slot_changed.bind(slot))
-		_slots.append(slot)
-	_slots.resize(number)
-	
-func _get_first_null_index() -> int:
-	## return -1 if don't have any null
-	for index in _slots.size():
-		if _slots[index] == null:
-			return index
-	return -1
+		_slots[i] = slot
+
 	
 ## OnSignals
 func _on_slot_changed(slot:InventorySlot):
